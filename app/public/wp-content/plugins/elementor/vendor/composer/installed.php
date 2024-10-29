fc57dd3101db7e598cd2adc9f1854ad083fe8b30
Rifqi Muhammad <rifqi.mhad@gmail.com>
2024-10-30 04:37:54 +0700

    30/10

---
+++ app/public/wp-content/plugins/elementor/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor',
+        'pretty_version' => '3.25.x-dev',
+        'version' => '3.25.9999999.9999999-dev',
+        'reference' => 'bb44d7bee3aecf9506f29d09c83e76c94c75df30',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor' => array(
+            'pretty_version' => '3.25.x-dev',
+            'version' => '3.25.9999999.9999999-dev',
+            'reference' => 'bb44d7bee3aecf9506f29d09c83e76c94c75df30',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
